232fa33ab0fcef6dda4c86b65d0a078e67213afa
Emile Thigpen <85521988+ethigpen@users.noreply.github.com>
2021-09-19 17:25:27 -0500

    small change

--- product_manager/server/config/config.js
+++ product_manager/server/config/config.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const db_name = "wall_of_ninjas_db";
 
 //mongoose connection/config
 mongoose.connect("mongodb+srv://root:rootroot@mernprojects.9thih.mongodb.net/productsdb?retryWrites=true&w=majority", {
